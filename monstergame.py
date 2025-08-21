e75d1f60fad56f212249c43bcfe752c2ae38b10e
Drone1112 <jamiedunnet@cromwell.school.nz>
2025-08-21 14:48:07 +1200

    chore:canged

--- monstergame.py
+++ monstergame.py
@@ -1,15 +1,15 @@
 import easygui as eg
 Monsters = {
-    "1": {"Name": "Stoneling", "Strength": 7, "Speed": 1, "Stealth": 25, "Cunning": 15},
-    "2": {"Name": "Vexscream", "Strength": 1, "Speed": 6, "Stealth": 21, "Cunning": 19},
-    "3": {"Name": "Dawnmirage", "Strength": 5, "Speed": 15, "Stealth": 18, "Cunning": 22},
-    "4": {"Name": "Blazegolem", "Strength": 15, "Speed": 20, "Stealth": 23, "Cunning": 6},
-    "5": {"Name": "Websnake", "Strength": 7, "Speed": 15, "Stealth": 10, "Cunning": 5},
-    "6": {"Name": "Moldvine", "Strength": 21, "Speed": 18, "Stealth": 14, "Cunning": 5},
-    "7": {"Name": "Vortexwing", "Strength": 19, "Speed": 13, "Stealth": 19, "Cunning": 2},
-    "8": {"Name": "Rotthing", "Strength": 16, "Speed": 7, "Stealth": 4, "Cunning": 12},
-    "9": {"Name": "Froststep", "Strength": 14, "Speed": 14, "Stealth": 17, "Cunning": 4},
-    "10": {"Name": "Wispghoul", "Strength": 17, "Speed": 19, "Stealth": 3, "Cunning": 2}
+"Stoneling": {"Strength": 7, "Speed": 1, "Stealth": 25, "Cunning": 15}, 
+"Vexscream": {"Strength": 1, "Speed": 6, "Stealth": 21, "Cunning": 19}, 
+"Dawnmirage": {"Strength": 5, "Speed": 15, "Stealth": 18, "Cunning": 22}, 
+"Blazegolem": {"Strength": 15, "Speed": 20, "Stealth": 23, "Cunning": 6}, 
+"Websnake": {"Strength": 7, "Speed": 15, "Stealth": 10, "Cunning": 5}, 
+"Moldvine": {"Strength": 21, "Speed": 18, "Stealth": 14, "Cunning": 5}, 
+"Vortexwing": {"Strength": 19, "Speed": 13, "Stealth": 19, "Cunning":2}, 
+"Rotthing": {"Strength": 16, "Speed": 7, "Stealth": 4, "Cunning":12}, 
+"Froststep": {"Strength": 14, "Speed": 14, "Stealth": 17, "Cunning":4}, 
+"Wispghoul": {"Strength": 17, "Speed": 19, "Stealth": 3, "Cunning":2}
 }
 
 while True:
@@ -22,7 +22,7 @@ while True:
         output = ""
         for monster_id, info in Monsters.items():
             output += (
-                f"\n#{monster_id} — {info['Name']}\n"
+                f"\nName: {monster_id} \n"
                 f"  Strength: {info['Strength']}\n"
                 f"  Speed:    {info['Speed']}\n"
                 f"  Stealth:  {info['Stealth']}\n"
@@ -42,7 +42,13 @@ while True:
 
     elif choice == "Delete Card":
          
-         print("viewcards")
+        delete = eg.enterbox("Enter the name of the Monster you want to delete:")
+        if delete in Monsters:
+            if eg.ynbox(f"Are you sure you want to delete '{delete}'?", "Confirm Delete"):
+                del Monsters[delete]
+                eg.msgbox(f"'{delete}' monster deleted.")
+        else:
+            eg.msgbox("Combo not found.")
 
     elif choice == "Exit":
 
