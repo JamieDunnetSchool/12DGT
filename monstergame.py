fb925fd21d14678757da44c68942663830b76a8e
Drone1112 <jamiedunnet@cromwell.school.nz>
2025-08-28 12:57:29 +1200

    chore: had a fight with my script for an hour and fixed the 0-25

--- monstergame.py
+++ monstergame.py
@@ -28,11 +28,11 @@ while True:
                 f"  Stealth:  {info['Stealth']}\n"
                 f"  Cunning:  {info['Cunning']}\n"
             )
-        eg.msgbox(output or "No monsters yet.", "Monster Cards")
+        eg.msgbox(output or "No monsters yet.", "View Cataloge")
 
     elif choice == "Add Card":
      
-        name = eg.enterbox("Enter the name of the new Monster:")
+        name = eg.enterbox("Enter the name of the new Monster:", "Add Monster Card")
         if name:
                 strength = int(eg.enterbox(f"Enter {name}'s Strength 0-25:"))
                 speed = int(eg.enterbox(f"Enter {name}'s Speed 0-25:"))
@@ -50,37 +50,65 @@ while True:
                 eg.msgbox(f"Monster '{name}' added successfully!")
 
     elif choice == "Search/Change":
-          
-        search = eg.enterbox("Enter the name of the monster to search:")
-        if search in Monsters:
-            monster = Monsters[search]
-            message = (
-                f"Name: {search}\n"
-                f"  Strength: {monster['Strength']}\n"
-                f"  Speed:    {monster['Speed']}\n"
-                f"  Stealth:  {monster['Stealth']}\n"
-                f"  Cunning:  {monster['Cunning']}\n"
-            )
-            eg.msgbox(message, "Monster Details")
+            search = eg.enterbox("Enter the name of the monster to search:", "Search/Edit")
+            if search in Monsters:
+                monster = Monsters[search]
+                message = (
+                    f"Name: {search}\n"
+                    f"  Strength: {monster['Strength']}\n"
+                    f"  Speed:    {monster['Speed']}\n"
+                    f"  Stealth:  {monster['Stealth']}\n"
+                    f"  Cunning:  {monster['Cunning']}\n"
+                )
+                eg.msgbox(message, "Monster Details")
 
-            if eg.ynbox("Do you want to change this monster?", "Edit Monster"):
-                try:
-                    strength = int(eg.enterbox("Enter new Strength 0-25:", default=str(monster["Strength"])))
-                    speed = int(eg.enterbox("Enter new Speed 0-25:", default=str(monster["Speed"])))
-                    stealth = int(eg.enterbox("Enter new Stealth 0-25:", default=str(monster["Stealth"])))
-                    cunning = int(eg.enterbox("Enter new Cunning 0-25:", default=str(monster["Cunning"])))
+                if eg.ynbox("Do you want to change this monster?", "Edit Monster"):
+                    try:
+                        # Strength validation loop
+                        while True:
+                            strength = int(eg.enterbox("Enter new Strength (0-25):", default=str(monster["Strength"])))
+                            if 0 <= strength <= 25:
+                                break
+                            else:
+                                eg.msgbox("Strength must be between 0 and 25.")
 
-                    Monsters[search] = {
-                        "Strength": strength,
-                        "Speed": speed,
-                        "Stealth": stealth,
-                        "Cunning": cunning
-                    }
-                    eg.msgbox("Monster updated successfully.")
-                except:
-                    eg.msgbox("Invalid input. Update cancelled.")
-        else:
-            eg.msgbox("Monster not found.")
+                        # Speed validation loop
+                        while True:
+                            speed = int(eg.enterbox("Enter new Speed (0-25):", default=str(monster["Speed"])))
+                            if 0 <= speed <= 25:
+                                break
+                            else:
+                                eg.msgbox("Speed must be between 0 and 25.")
+
+                        # Stealth validation loop
+                        while True:
+                            stealth = int(eg.enterbox("Enter new Stealth (0-25):", default=str(monster["Stealth"])))
+                            if 0 <= stealth <= 25:
+                                break
+                            else:
+                                eg.msgbox("Stealth must be between 0 and 25.")
+
+                        # Cunning validation loop
+                        while True:
+                            cunning = int(eg.enterbox("Enter new Cunning (0-25):", default=str(monster["Cunning"])))
+                            if 0 <= cunning <= 25:
+                                break
+                            else:
+                                eg.msgbox("Cunning must be between 0 and 25.")
+
+                        # Save updates
+                        Monsters[search] = {
+                            "Strength": strength,
+                            "Speed": speed,
+                            "Stealth": stealth,
+                            "Cunning": cunning
+                        }
+                        eg.msgbox("Monster updated successfully.", "Success")
+
+                    except Exception:
+                        eg.msgbox("Invalid input. Update cancelled.", "Error")
+            else:
+                eg.msgbox("Monster not found.")
 
     elif choice == "Delete Card":
          
