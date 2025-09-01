ac71af49397381edf2a6cf976c09b363c4006be5
Drone1112 <jamiedunnet@cromwell.school.nz>
2025-09-01 12:27:38 +1200

    chore: new infomation and wof

--- monstergame.py
+++ monstergame.py
@@ -1,26 +1,22 @@
 import easygui as eg
 Monsters = {
-"Stoneling": {"Strength": 7, "Speed": 1, "Stealth": 25, "Cunning": 15}, 
-"Vexscream": {"Strength": 1, "Speed": 6, "Stealth": 21, "Cunning": 19}, 
-"Dawnmirage": {"Strength": 5, "Speed": 15, "Stealth": 18, "Cunning": 22}, 
-"Blazegolem": {"Strength": 15, "Speed": 20, "Stealth": 23, "Cunning": 6}, 
-"Websnake": {"Strength": 7, "Speed": 15, "Stealth": 10, "Cunning": 5}, 
-"Moldvine": {"Strength": 21, "Speed": 18, "Stealth": 14, "Cunning": 5}, 
-"Vortexwing": {"Strength": 19, "Speed": 13, "Stealth": 19, "Cunning":2}, 
-"Rotthing": {"Strength": 16, "Speed": 7, "Stealth": 4, "Cunning":12}, 
-"Froststep": {"Strength": 14, "Speed": 14, "Stealth": 17, "Cunning":4}, 
-"Wispghoul": {"Strength": 17, "Speed": 19, "Stealth": 3, "Cunning":2}
+    "Stoneling": {"Strength": 7, "Speed": 1, "Stealth": 25, "Cunning": 15},
+    "Vexscream": {"Strength": 1, "Speed": 6, "Stealth": 21, "Cunning": 19},
+    "Dawnmirage": {"Strength": 5, "Speed": 15, "Stealth": 18, "Cunning": 22},
+    "Blazegolem": {"Strength": 15, "Speed": 20, "Stealth": 23, "Cunning": 6},
+    "Websnake": {"Strength": 7, "Speed": 15, "Stealth": 10, "Cunning": 5},
+    "Moldvine": {"Strength": 21, "Speed": 18, "Stealth": 14, "Cunning": 5},
+    "Vortexwing": {"Strength": 19, "Speed": 13, "Stealth": 19, "Cunning":2},
+    "Rotthing": {"Strength": 16, "Speed": 7, "Stealth": 4, "Cunning":12},
+    "Froststep": {"Strength": 14, "Speed": 14, "Stealth": 17, "Cunning":4},
+    "Wispghoul": {"Strength": 17, "Speed": 19, "Stealth": 3, "Cunning":2}
 }
-
-
-
 while True:
-
-    choice = eg.buttonbox("Welcome to the Monster game what would you like to do.", "Selection", 
+    choice = eg.buttonbox("Welcome to the Monster game what would you like to do.", "Selection",
                           choices=["View Cards", "Add Card", "Search/Change", "Delete Card", "Exit"])
-    
-    if choice == "View Cards":
 
+    # View cards
+    if choice == "View Cards":
         output = ""
         for monster_id, info in Monsters.items():
             output += (
@@ -31,13 +27,10 @@ while True:
                 f"  Cunning:  {info['Cunning']}\n"
             )
         eg.msgbox(output or "No monsters yet.", "View Cataloge")
-
-    elif choice == "Add Card":
-     
-
-     
-        name = eg.enterbox("Enter the name of the new Monster:", "Add Monster Card")
         
+# Add cards 
+    elif choice == "Add Card":
+        name = eg.enterbox("Enter the name of the new Monster:", "Add Monster Card")
         while True:
                     strength = int(eg.enterbox(f"Enter {name}'s Strength 0-25:"))
                     if 0 <= strength <= 25:
@@ -50,24 +43,35 @@ while True:
                                 break
                     else:
                         eg.msgbox("Strength must be between 0 and 25.")
-        while True:      
+        while True:
                     stealth = int(eg.enterbox(f"Enter {name}'s Stealth 0-25:"))
                     if 0 <= stealth <= 25:
                                 break
                     else:
                         eg.msgbox("Strength must be between 0 and 25.")
-        while True:        
+        while True:
                     cunning = int(eg.enterbox(f"Enter {name}'s Cunning 0-25:"))
                     if 0 <= cunning <= 25:
                                 break
                     else:
                         eg.msgbox("Strength must be between 0 and 25.")
+            
+        verify = (
+                f"Are you sure you want to add this monster?\n\n"
+                f"Name: {name}\n"
+                f"  Strength: {strength}\n"
+                f"  Speed:    {speed}\n"
+                f"  Stealth:  {stealth}\n"
+                f"  Cunning:  {cunning}"
+            )
+            
+        if eg.ynbox(verify, "Verify Monster"):
+                Monsters[name] = {"Strength": strength, "Speed": speed, "Stealth": stealth, "Cunning": cunning}
+                eg.msgbox(f"Monster '{name}' added successfully!")
+        else:
+                eg.msgbox("Monster not added.")
 
-        Monsters[name] = {"Strength": strength, "Speed": speed, "Stealth": stealth, "Cunning": cunning}
-
-        eg.msgbox(f"Monster '{name}' added successfully!")
-
-
+#Search and Edit cards
     elif choice == "Search/Change":
             search = eg.enterbox("Enter the name of the monster to search:", "Search/Edit")
             if search in Monsters:
@@ -128,28 +132,25 @@ while True:
                         eg.msgbox("Invalid input. Update cancelled.", "Error")
             else:
                 eg.msgbox("Monster not found.")
-
+# Delete Card
     elif choice == "Delete Card":
                 
                 output2 = ""
                 for monster_id, info in Monsters.items():
-                    output2 += f"Name: {monster_id}\n"   
-
+                    output2 += f"Name: {monster_id}\n"
 
                 delete = eg.enterbox(
                     "Enter the name of the Monster you want to delete:\n\n" + output2,
                     title="Delete"
                 )
 
-
-                    
                 if delete in Monsters:
                         if eg.ynbox(f"Are you sure you want to delete '{delete}'?", "Confirm Delete"):
                             del Monsters[delete]
                             eg.msgbox(f"'{delete}' monster deleted.")
                 else:
                     eg.msgbox("Monster not found.")
-
+# Exit 
     elif choice == "Exit":
 
-         break   
+         break
